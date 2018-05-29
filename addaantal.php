16b9d78fd3146806c3d5d32d7662a95f8ca98266
themilanguy1 <milan.gupta@ymail.com>
2018-05-29 16:57:19 +0200

    addaantal nu ook distinct input

--- addaantal.php
+++ addaantal.php
@@ -3,17 +3,22 @@ include 'connectlocal.php';
 
 $stad = $_POST['aantalstadinput'];
 $winkel = $_POST['aantalwinkelinput'];
-$aantal = $_POST['aantalnummerinput'];
-$sql = "INSERT INTO stad_winkel (stad_id, winkel_id, aantal_filialen)
-VALUES ('$stad', '$winkel', '$aantal')";
+$query = mysqli_query($conn, "SELECT stad_id, winkel_id FROM stad_winkel WHERE stad_id='".$stad."' AND winkel_id ='".$winkel."' ");
+    if (!$query) {
+        die('Error: ' . mysqli_error($conn));
+    }
+if(mysqli_num_rows($query) > 0){
+    } else {
+        $aantal = $_POST['aantalnummerinput'];
+        $sql = "INSERT INTO stad_winkel (stad_id, winkel_id, aantal_filialen)
+        VALUES ('$stad', '$winkel', '$aantal')";
 
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+        
+        $conn->close();
 }
-
-$conn->close();
-
 header('Location: SQLhome.php');
-
